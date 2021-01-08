0f650cf72df44bb450c9f42d8f2c3d508d88b79e
cadenadevalor <76475718+cadenadevalor@users.noreply.github.com>
2021-01-08 10:21:16 +0100

    Add files via upload

---
+++ data/Pasta_15.js
@@ -0,0 +1,8 @@
+var json_Pasta_15 = {
+"type": "FeatureCollection",
+"name": "Pasta_15",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "258", "Nombre": "Jornadas de la Fideuà", "Gestión": null, "Descrip": "Jornadas gastronómicas en las que se pueden degustar varias especialidades de esta tradicional receta valenciana a precios asequibles.\n", "Dirección": "Ubicación representativa", "Localidad": "Vinaròs\n", "Producto": "P-Pasta", "Temporada": "Primavera\n", "Web Info": "http:\/\/turisme.vinaros.es\n", "Categoría": "Feria, muestra o evento" }, "geometry": { "type": "Point", "coordinates": [ 0.478425430931325, 40.471856288311798 ] } }
+]
+}
---
+++ data/Salsas_14.js
@@ -0,0 +1,8 @@
+var json_Salsas_14 = {
+"type": "FeatureCollection",
+"name": "Salsas_14",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "74", "Nombre": "Concurso de Pebrereta", "Gestión": "La Vila Gastronòmica (Ayuntamiento de La Vila Joiosa)\n", "Descrip": "Evento que se realiza en la Plaza de la Llum y que reúne chef de esta receta local, consistente en pimientos, ajos, calabaza, sal, pimentón dulce y sangatxo. Plato de pescador y de pobre.", "Dirección": "Pl. de la Llum\n", "Localidad": "La Vila Joiosa\n", "Producto": "S-Salsas", "Temporada": "Junio\n", "Web Info": "www.lavilagastronomica.com\/v1\/pebrereta\/default.php\n", "Categoría": "Feria, muestra o evento" }, "geometry": { "type": "Point", "coordinates": [ -0.235840426416077, 38.503021343668848 ] } }
+]
+}
